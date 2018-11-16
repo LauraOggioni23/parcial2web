0fd6bd390da11f3ee9f4155ccb1db850d71e6f8e
Laura Oggioni Reyes <1144095614@academico.icesi.edu.co>
2018-11-16 08:58:53 -0500

    render de paginas y demas

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const express = require('express');
-const hbs = require('express-handlebars');
+const express = require('express'); //Express
+const hbs = require('express-handlebars'); 
 const app = express();
-const fs = require('fs');
+const fs = require('fs'); //File system
 const download = require('download');
-const fs = require('fs');
 
-//para definir la carpeta publica
+var first = 0; //Visitas primera pagina
+var second= 0; //visitas segunda pagina
+var third= 0; //visitas tercera pagina
+
+function visits(params) {
+    var message = "first: "+first;
+    console.log(message);
+    
+}
+
+//carpeta publica
 app.use(express.static('public'));
-//para registrar el motor de render handlebars
+//render handlebars
 app.engine('handlebars', hbs());
-//para setear el motor de render a utilizar
+//setear el motor de render
 app.set('view engine', 'handlebars');
+
+
+//primera pagina
+app.get('/first',function(req , res){
+    res.render('first');                    
+});
+
+//segunda pagina
+app.get('/second',function(req , res){
+    res.render('second');                    
+});
+
+//tercera pagina
+app.get('/third',function(req , res){
+    res.render('third');                    
+});
+
+
+
+
+
+app.listen(5500); //Puerto
