ff2110eb39112ab1f560df37b6cb615b61ce4837
Laura Oggioni Reyes <1144095614@academico.icesi.edu.co>
2018-11-16 08:16:14 -0500

    primera parte

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const app = express();
+const fs = require('fs');
+const download = require('download');
+
+//para definir la carpeta publica
+app.use(express.static('public'));
+//para registrar el motor de render handlebars
+app.engine('handlebars', hbs());
+//para setear el motor de render a utilizar
+app.set('view engine', 'handlebars');
---
+++ public/first/script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load',function(){
+    var urlimagen = document.querySelector('.url');
+    var nombre = document.querySelector('.nombre');
+})
